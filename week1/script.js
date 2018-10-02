58b59f693478f28198b9b3e269314973a25e0f2a
Farshid Farjad <ffarjad@gmail.com>
2018-10-02 22:21:50 +0200

    update02

--- week1/script.js
+++ week1/script.js
@@ -12,3 +12,19 @@ console.log(x);
 x = 5;
 console.log('I think the value of x is 5 ');
 console.log('the real value is: ' + x);
+
+//4.assign different values to String variable
+var y = 'Farshid';
+console.log('I think the value of y is: Farshid')
+console.log(y);
+y = 'Farjad';
+console.log('I think the value of y now is: Farjad');
+console.log(y);
+
+//5.maximum number
+var z = 7.25;
+console.log(z);
+var a = parseInt(7.25);
+console.log(a);
+var highest_value = Math.max(a, z); 
+console.log("the highest value between " + z + " and " + parseInt(z) + " is " + highest_value);
