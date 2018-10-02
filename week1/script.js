9e876afc26419c022d995e16bcc33228872eab92
Farshid Farjad <ffarjad@gmail.com>
2018-10-02 22:12:52 +0200

    Update01

--- week1/script.js
+++ week1/script.js
@@ -1,2 +1,14 @@
-// 1 print hello world
+//1.Write hello world
 console.log('Hello world!');
+
+//2.SyntaxError
+console.log('I\'m awesome');
+console.log("I'm awesome");
+
+//3.Declare a variable
+var x;
+console.log('the value of my variable x will be: 5');
+console.log(x);
+x = 5;
+console.log('I think the value of x is 5 ');
+console.log('the real value is: ' + x);
